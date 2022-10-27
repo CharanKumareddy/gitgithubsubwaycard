336febb5fd453d8e80e69c4b384d23ae6dcab926
CharanKumareddy <charansmart26@gmail.com>
2022-10-27 10:10:22 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,76 @@
+let text = "charan";
+
+text ? console.log(`this is truthy ${text}`) : console.log("this is falsy");
+
+
+
+
+// let text = "charan";
+// if(text){
+//     console.log(`this is truthy ${text}`);
+// }
+// else{
+//     console.log("this is falsy");
+// }
+
+
+
+
+// let num1 = 10;
+// let num2 = num1;
+// num2 = 20;
+// console.log(`the number is ${num1}`);
+// console.log(`the number is ${num2}`);
+
+
+// let person1 ={ name1:"charan"};
+// let person2 ={...person1};
+// person2.name1 = "harry"
+
+// console.log(`person name is ${person1.name1}`);
+// console.log(`person name is ${person2.name1}`);
+
+
+// let mobileReacharge = [200,300,400];
+
+// function calculateTotal(arr){
+//     let total = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         total += arr[i];
+//         console.log(i);
+//         console.log(arr[i]);
+        
+//         console.log(total);
+//         if(total>500){
+//             console.log("your paying high amount");
+//         }
+        
+//         else{
+//             console.log("your paying good amount ");
+//         }
+//     }
+
+    
+
+// }
+// // console.log(){
+
+// // };
+
+// calculateTotal(mobileReacharge);
+
+
+
+
+
+
+
+
+// let firstName = ["charan","sanath","tharun","vinodh"];
+// let lastName = ["kaipu","boyapati","gangala","bodireddy"];
+// let result = [];
+// for (let i=0;i<firstName.length;i++){
+//     let allNames = `${lastName[i]} ${firstName[i]}`
+//     result.push(allNames);
+// }
+// console.log(result);
